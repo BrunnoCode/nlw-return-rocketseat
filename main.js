a97145df90ed8f850877820ef8d6046b4b46d574
Brunno-rocha <brunnowr.bw@gmail.com>
2022-05-11 22:33:58 +0200

    nlw-upload-custom-color

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
+
+window.addEventListener('scroll', onScroll)
+
+
+
+
+onScroll()
 function onScroll() {
- 
-if (scrollY > 0) {
-    navigation.classList.add('scroll')
-} else {
-        navigation.classList.remove('scroll')
-    }
+ showNavOnScroll()
+ showBackToTopButtonOnScroll()
+}
+
+function showNavOnScroll() {
+    if (scrollY > 0) {
+        navigation.classList.add('scroll')
+    } else {
+            navigation.classList.remove('scroll')
+        }
+}
+
+function showBackToTopButtonOnScroll() {
+    if (scrollY > 500) {
+        backToTopButton.classList.add('show')
+    } else {
+        backToTopButton.classList.remove('show')
+        }
 }
 
 function openMenu() {
